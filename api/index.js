6ea06653922c7ab45bfe4c77b4d12143c2405193
fimaAidarous <fma0899@gmail.com>
2024-05-13 08:09:10 -0700

    create a middleware and a fucntion to handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,14 @@ app.listen (500, () => {
 
 app.use("/api/user", userRouter);
 app.use('/api/auth',autherRouter);
+
+// Middleware
+app.use((err, req,res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
